728b7bd3d1fd5acdf149091eb6d64c23222f7bcb
Raphael <phael92br@gmail.com>
2023-03-27 11:11:39 -0300

    first commit

---
+++ src/sections/FAQ/FAQ.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './faq.css'
+
+const FAQ = () => {
+  return (
+    <section id="faq"><h2>FAQ</h2></section>
+  )
+}
+
+export default FAQ
---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,9 @@
+import  './floating-nav.css'
+
+const FloatingNav = () => {
+  return (
+    <section id="floating__nav"><h2>FloatingNav</h2></section>
+  )
+}
+
+export default FloatingNav
