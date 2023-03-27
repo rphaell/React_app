528a33df22a9fc552880291fedca8a27f541bb26
Raphael <phael92br@gmail.com>
2023-03-27 17:49:25 -0300

    navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
+import Logo from '../../assets/logo.jpg'
+import data from './data'
 import './navbar.css'
+import {MdDarkMode} from 'react-icons/md'
+
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className='container nav__container'>
+        <a href= "index.html" className = "nav__logo">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav__menu'>
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id = 'theme__icon'><MdDarkMode/></button>
+      </div>
+    </nav>
   )
 }
 
